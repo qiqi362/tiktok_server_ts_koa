38526831c6eaf73450d684d653a81e55f009a777
梁壮其 <lzq@juhua.com>
2024-08-22 17:21:41 +0800

    feat：注册加密与登录验证

--- src/api/system/auth/login.ts
+++ src/api/system/auth/login.ts
@@ -3,23 +3,29 @@ import { Models } from '../../../models/mysql-model'
 import { command } from '../../../server/mysql'
 import { Success, AuthFailed } from '../../../core/HttpException'
 import { generateToken } from '../../../server/auth/token'
+import bcrypt from 'bcryptjs'
 
 interface userType {
-  id?: number
+  id?: number,
+  password?: string
 }
-const login = (results: Array<userType>) => {
-  if (results.length === 0) return new AuthFailed('账号密码错误')
-  const user = results[0] || { id: null }
+const login = async (password: string, results: Array<userType>) => {
+  const user = results[0] || { id: null, password: null }
+  if(!user.password) return new AuthFailed('密码为空')
+  const isMatch = await bcrypt.compare(password, user.password)
+  if (!isMatch) return new AuthFailed('账号密码错误')
   const token = user.id && generateToken(user.id)
   return new Success(token)
 }
 const router = new Router({prefix: '/api/system/auth'})
 router.get('/login', async (ctx: Models.Ctx) => {
-  const { password, username } = ctx.query
+  let { password = '', username } = ctx.query
+  password = password||password[0]||''
   const loginRes = (
-    await command(`select id from user where username = '${username}' and password = '${password}'`)
+    await command(`select * from user where username = '${username}'`)
   ).results
-  const res = login(loginRes)
+  if(Array.isArray(password)||!password) return
+  const res = await login(password, loginRes)
   ctx.body = res
 })
 
--- src/api/system/auth/registe.ts
+++ src/api/system/auth/registe.ts
@@ -3,18 +3,23 @@ import { Models } from '../../../models/mysql-model'
 import { command } from '../../../server/mysql'
 import { Success, DataExistFailed } from '../../../core/HttpException'
 import dayjs from 'dayjs'
+import bcrypt from 'bcryptjs'
+
 const router = new Router({ prefix: '/api/system/auth' })
 
 const registe = async(password: string, username: string, results: Array<object>) => {
   if (results?.length > 0) return new DataExistFailed()
   const createTime = dayjs().format('YYYY-MM-DD HH:MM:ss')
+  const hashPassword = await bcrypt.hash(password, 10)
   const insertUserResults = (
-    await command(`insert into user (username, password, create_time, register_time, update_time) values ('${username}', '${password}', '${createTime}', '${createTime}', '${createTime}')`)
+    await command(`insert into user (username, password, create_time, register_time, update_time) values ('${username}', '${hashPassword}', '${createTime}', '${createTime}', '${createTime}')`)
   ).results
   return new Success(insertUserResults)
 }
 
 router.post('/registe', async (ctx: Models.Ctx) => {
+  console.log(77777, ctx);
+  
   const { password, username } = ctx.request.body
   const results = (
     await command(`select * from user where username='${username}'`)
